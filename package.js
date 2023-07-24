db8f6f6a96bb335b78cfe2eb0e5f86ff3fa6df29
punishell <33149001+punishell@users.noreply.github.com>
2023-07-24 17:26:18 +0200

    Create package.js

---
+++ package.js
@@ -0,0 +1,19 @@
+{
+  "name": "kujs",
+  "version": "1.0.0",
+  "description": "",
+  "main": "index.js",
+  "scripts": {
+    "test": "node index.js"
+  },
+  "author": "",
+  "license": "ISC",
+  "dependencies": {
+    "@cosmjs/cosmwasm-stargate": "^0.28.13",
+    "@cosmjs/launchpad": "^0.27.1",
+    "@cosmjs/math": "^0.28.13",
+    "@cosmjs/proto-signing": "^0.28.13",
+    "@cosmjs/stargate": "^0.28.13",
+    "kujira.js": "git+https://github.com/Team-Kujira/kujira.js.git"
+  }
+}
